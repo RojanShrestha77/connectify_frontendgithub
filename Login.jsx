9c6b5906595cf239411dd3381b7be4c7a7081df3
RojanShrestha77 <151616745+RojanShrestha77@users.noreply.github.com>
2025-02-15 13:00:27 +0545

    Delete Login.jsx

--- Login.jsx
+++
@@ -1,8 +0,0 @@
-function Login(){
-    return(
-    <div className="bg_image">
-        <img src={back} alt="background"></img>
-        <h1 className="connectify-logo"></h1>
-      </div>)
-}
-export default Login;
